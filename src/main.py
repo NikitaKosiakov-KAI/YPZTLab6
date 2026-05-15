fadefc37c51ede37a6b64f2571eb3df4ffb45ab4
NK-KAI <8666975@stud.kai.edu.ua>
2026-05-15 18:24:00 +0300

    Added main code

---
+++ src/main.py
@@ -0,0 +1,13 @@
+def add_numbers(a, b):
+    """Функція для додавання двох чисел."""
+    return a + b
+
+def multiply_numbers( a, b ):
+    result=a*b
+    return result
+
+def is_even(number):
+  if number%2==0:
+    return True
+  else:
+    return False
